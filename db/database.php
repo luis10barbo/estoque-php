1282b4393afb5216c4fb6f20a70a3195d12b6885
luis10barbo <luis10barbo2020@gmail.com>
2023-06-29 14:18:38 -0300

    Se nao existir, Criar DB com schema

--- db/database.php
+++ db/database.php
@@ -1,5 +1,7 @@
 <?php
 require_once(__DIR__ . "/tabelas/sessao.php");
+$diretorio_db = __DIR__ . "/database.db";
+
 class Database
 {
     private static PDO $db;
@@ -8,17 +10,28 @@ class Database
     private function __construct()
     {
     }
-
+    private static function setar_schema_db()
+    {
+        $query = file_get_contents(__DIR__ . "/creation.sql");
+        self::$db->exec($query);
+    }
     private static function adquirir_db()
     {
+        global $diretorio_db;
         if (!isset(self::$db)) {
+            $setar_schema = !file_exists($diretorio_db);
+
             try {
-                self::$db = new PDO("sqlite:" . __DIR__ . "/database.db");
+                self::$db = new PDO("sqlite:" . $diretorio_db);
             } catch (PDOException $e) {
                 $error = $e->getCode();
                 var_dump($error);
                 exit();
             }
+
+            if ($setar_schema)
+                self::setar_schema_db();
+
         }
 
         return self::$db;
