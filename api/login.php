7f26c1a7531abc830cd970974e4ccd7cd37b4bac
vinicius <vlconta.suporte@gmail.com>
2023-07-04 23:27:31 -0300

    Criada rota login

--- api/login.php
+++ api/login.php
@@ -1,4 +1,10 @@
 <?php
+
+require_once(__DIR__."/../db/database.php");
+require_once(__DIR__."/../utils/error.php");
+require_once(__DIR__."/../utils/sessao.php");
+
+
 if (!isset($_POST["email"]) || !isset($_POST["password"]))
     return;
 
@@ -8,6 +14,20 @@ $senha = $_POST["password"];
 // Buscar o usuario pelo email
 // checar se a senha enviada corresponde ao hash guardado no usuario
 // caso sim, atualizar id_usuario na sessao
+
+$resultado = Database::usuario()->buscar_email($email);
+
+if(!$resultado){
+    echo criar_erro("Email nao existe");
+    return;
+}
+if(!password_verify($senha, $resultado["senha_usuario"])){
+    echo criar_erro("Senha incorreta");
+    return;
+}
+
+$sessao = adquirir_sessao();
+Database::sessao()->atualizar($sessao["id_sessao"],$resultado["id_usuario"]);
 
 echo "Email: " . $email . "Senha:" . $senha;
 ?>
