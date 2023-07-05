33ef40bd36da962bb4478fa789e2b004f07081c5
luis10barbo <luis10barbo2020@gmail.com>
2023-07-04 22:55:57 -0300

    Adicionadas instrucoes para login

--- api/login.php
+++ api/login.php
@@ -5,5 +5,9 @@ if (!isset($_POST["email"]) || !isset($_POST["password"]))
 $email = $_POST["email"];
 $senha = $_POST["password"];
 
+// Buscar o usuario pelo email
+// checar se a senha enviada corresponde ao hash guardado no usuario
+// caso sim, atualizar id_usuario na sessao
+
 echo "Email: " . $email . "Senha:" . $senha;
 ?>
